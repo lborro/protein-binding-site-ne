850907ab446c58350e15b3bf78cb504989a7a39b
Luiz Cesar Borro <lborro@gmail.com>
2017-12-05 14:37:07 -0200

    binding-site nano-environment calculation

--- src/br/embrapa/cnptia/gpbc/plc/calculators/NanoEnvironmentCalculator.java
+++ src/br/embrapa/cnptia/gpbc/plc/calculators/NanoEnvironmentCalculator.java
@@ -10,14 +10,17 @@ public class NanoEnvironmentCalculator implements Callable<ProteinNanoEnvironmen
 	private final File protein, ligand;
 	private final MaxContactsTable maxCon;
 	private final double dist;
+	private final String epDir;
 
-	public NanoEnvironmentCalculator (File pdbFile, File mol2File, MaxContactsTable maxCon, double dist) throws Exception {
+	public NanoEnvironmentCalculator (File pdbFile, File mol2File, MaxContactsTable maxCon, String epDir, double dist) throws Exception {
 		if (pdbFile == null) 
 			throw new NullPointerException("PDB file describing the protein cannot be null !");
 		if (mol2File == null) 
 			throw new NullPointerException("Mol2 file describing the ligand cannot be null !");
 		if (maxCon == null) 
 			throw new NullPointerException("Table of maximum contacts per AA cannot be null!");
+		if (epDir == null) 
+			throw new NullPointerException("EP dir cannot be null!");	
 		if(dist <= 0 || dist > 12)
 			throw new Exception("Distance must be greater than zero and less or equal than 12.0");
 
@@ -25,13 +28,14 @@ public class NanoEnvironmentCalculator implements Callable<ProteinNanoEnvironmen
 		this.ligand = mol2File;
 		this.maxCon = maxCon;
 		this.dist = dist;
+		this.epDir = epDir;
 	}
 
 	@Override
 	public ProteinNanoEnvironment call() throws Exception {
 		try{
 			ProteinLigandComplex complex = new ProteinLigandComplex(protein, ligand, 12);
-			ProteinNanoEnvironment nano = new ProteinNanoEnvironment(complex, maxCon);
+			ProteinNanoEnvironment nano = new ProteinNanoEnvironment(complex, maxCon, epDir);
 			nano.calculate(dist);
 			return nano;
 		} catch (Exception e) {
